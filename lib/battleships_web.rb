d5a85a8f5e3e5493110416f6636e97f04b5acd24
ana <adscarlos@gmail.com>
2015-09-17 17:16:34 +0100

    refactors controller

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -13,17 +13,15 @@ class BattleshipsWeb_Ana < Sinatra::Base
     erb :index
   end
 
-  get '/new_game' do
+  post '/new_game' do
+    session[:name] ||= params[:player]
+    redirect to '/' if !session[:name] || session[:name] == ''
     @name = session[:name]
-    session[:board] = Board.new(Cell)
+    session[:board] ||=  Board.new(Cell)
     @board = session[:board].grid
     erb :new_game
   end
 
-  post '/new_game' do
-    session[:name] = params[:player]
-    redirect to('/new_game')
-  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -4,28 +4,28 @@ feature 'Starting a new game' do
 
   scenario 'I am asked to enter my name' do
     visit '/'
-    click_link 'New Game'
-    expect(page).to have_content "What's your name?"
+    click_button 'Play!'
+    expect(page).to have_content "what's your name?"
   end
 
   scenario 'I can my enter my name' do
-    visit '/new_game'
+    visit '/'
     fill_in 'player', :with => 'Michael'
-    click_button 'Enter'
+    click_button 'Play!'
     expect(page).to have_content "Welcome to the game, Michael!"
   end
 
   scenario 'Asks for an actual name to be entered' do
     visit '/new_game'
     fill_in 'player', :with => ''
-    click_button 'Enter'
-    expect(page).to have_content "What's your name?"
+    click_button 'Play!'
+    expect(page).to have_content "what's your name?"
   end
 
   scenario 'Shows a battleships board to new player' do
     visit '/new_game'
     fill_in 'player', :with => 'Michael'
-    click_button 'Enter'
+    click_button 'Play!'
     expect(page).to have_content 'J10'
   end
 
