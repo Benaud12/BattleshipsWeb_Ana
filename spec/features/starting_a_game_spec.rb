7b7717f182b161202ee9cb5e2744c21a37c944ce
Anindya Bhattacharyya <bat020@gmail.com>
2015-09-18 16:58:24 +0100

    escapes from place ships page when all 5 are placed

--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 feature 'Starting a new game' do
 
+  before do
+    $board = Board.new(Cell)
+  end
+
   scenario 'I am asked to enter my name' do
     visit '/'
     click_button 'Play!'
@@ -38,4 +42,29 @@ feature 'Starting a new game' do
     expect(page).to have_content("~~", :count => 95)
   end
 
+  scenario 'it updates the message with number of ships left to place' do
+    visit '/'
+    fill_in 'player', :with => 'Michael'
+    click_button 'Play!'
+    fill_in 'position', :with => 'A1'
+    click_button 'Submit'
+    expect(page).to have_content "You have 4 ship(s) left to place."
+  end
+
+  scenario 'it displays a start game button when all ships placed' do
+    visit '/'
+    fill_in 'player', :with => 'Michael'
+    click_button 'Play!'
+    fill_in 'position', :with => 'A1'
+    click_button 'Submit'
+    fill_in 'position', :with => 'B1'
+    click_button 'Submit'
+    fill_in 'position', :with => 'C1'
+    click_button 'Submit'
+    fill_in 'position', :with => 'D1'
+    click_button 'Submit'
+    fill_in 'position', :with => 'E1'
+    click_button 'Submit'
+    expect(page).to have_selector "input[type=submit][value='Start the game!']"
+  end
 end
